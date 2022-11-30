e5dec380b1458bea2f8c0beeb23e7d5b02890cfb
acoetzer <coetzeronline@gmail.com>
2022-11-30 16:24:30 +0200

    finished the moile navigation

--- resources/javascript/app.js
+++ resources/javascript/app.js
@@ -1,5 +1,5 @@
 ////////////////////////////////////////
-// Making background blurred
+// Making the header sticky
 
 const sectionHeroEl = document.querySelector('.section-hero');
 const headerEl = document.querySelector('.header');
@@ -21,5 +21,18 @@ const obs = new IntersectionObserver((entries) => {
 obs.observe(sectionHeroEl)
 
 ////////////////////////////////////////
-// Making the header sticky
+// Making the mobile nav active
 
+const mobileBtnEl = document.querySelector('.mobile-nav');
+
+mobileBtnEl.addEventListener('click', () => {
+  document.body.classList.toggle('mobile-active');
+})
+
+////////////////////////////////////////
+// Copyright Date Changer, it updates the date automatically
+
+const copyrightEl = document.querySelector('#copyright');
+
+const newDate = new Date().getFullYear();
+copyrightEl.innerHTML = newDate;
