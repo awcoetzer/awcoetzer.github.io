22149d50444bc319d05810e36726f3946db24719
acoetzer <coetzeronline@gmail.com>
2022-12-05 12:20:02 +0200

    fixed a lot fo things prepped first lecture

--- resources/javascript/app.js
+++ resources/javascript/app.js
@@ -1,39 +1,42 @@
 ////////////////////////////////////////
 // Making the header sticky
 
-const sectionHeroEl = document.querySelector('.section-hero');
-const headerEl = document.querySelector('.header');
+const sectionHeroEl = document.querySelector(".section-hero");
+const headerEl = document.querySelector(".header");
 
-const obs = new IntersectionObserver((entries) => {
-  const ent = entries[0];
+const obs = new IntersectionObserver(
+  (entries) => {
+    const ent = entries[0];
 
-  if (ent.isIntersecting === false) {
-    document.body.classList.add('sticky');
-  } else {
-    document.body.classList.remove('sticky');
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    } else {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
   }
-}, {
-  root:null,
-  threshold: 0,
-  rootMargin: '-80px',
-})
+);
 
-obs.observe(sectionHeroEl)
+obs.observe(sectionHeroEl);
 
 ////////////////////////////////////////
 // Making the mobile nav active
 
-const mobileBtnEl = document.querySelector('.mobile-nav');
+const mobileBtnEl = document.querySelector(".mobile-nav");
 
-mobileBtnEl.addEventListener('click', () => {
-  document.body.classList.toggle('mobile-active');
-})
+mobileBtnEl.addEventListener("click", () => {
+  document.body.classList.toggle("mobile-active");
+});
 
 ////////////////////////////////////////
 // Making the toc clickable to display its contents
 
-const tocEl = document.querySelector('.toc');
-console.log(tocEl)
+const tocEl = document.querySelector(".toc");
+console.log(tocEl);
 // tocEl.addEventListener('click', () => {
 //   document.body.classList.toggle('mobile-active');
 // })
@@ -41,7 +44,7 @@ console.log(tocEl)
 ////////////////////////////////////////
 // Copyright Date Changer, it updates the date automatically
 
-const copyrightEl = document.querySelector('#copyright');
+const copyrightEl = document.querySelector("#copyright");
 
 const newDate = new Date().getFullYear();
 copyrightEl.innerHTML = newDate;
