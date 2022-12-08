344d893cc3b3aa51ec081991187c421888fdacfb
acoetzer <coetzeronline@gmail.com>
2022-12-08 20:11:45 +0200

    section-2 lecture 9 and 10 Completed also added new js file for the section as practice added some new styles as well

---
+++ resources/javascript/javascript-fundamentals-part-1.js
@@ -0,0 +1,21 @@
+// Lecture 9
+console.log('LECTURE 9');
+console.log('👋 This is how to log to the console!');
+console.log('');
+
+// Lecture 10
+console.log('LECTURE 10');
+console.log('Andre');
+
+let firstName = 'Andre';
+console.log(firstName);
+console.log('');
+
+// Lecture 10 - Assignment
+console.log('LECTURE 10 - ASSIGNMENT');
+const country = 'Bulgaria';
+const continent = 'Europe';
+let population = 6899000;
+
+console.log(`${country}, ${continent}, ${population}`)
+console.log('');
