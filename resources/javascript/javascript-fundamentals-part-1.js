011c8c339c64b1278de80dff3c791198cc69f797
acoetzer <coetzeronline@gmail.com>
2022-12-09 21:25:22 +0200

    Coding Challenge - 2 Completed

--- resources/javascript/javascript-fundamentals-part-1.js
+++ resources/javascript/javascript-fundamentals-part-1.js
@@ -197,3 +197,14 @@ if (population > averagePopulation) {
 }
 
 console.log("");
+
+// Coding challenge - 2
+console.log("Coding challenge - 2");
+
+if (bmiMark > bmiJohn) {
+  console.log(`Mark's BMI(${bmiMark}) is higher than John's BMI(${bmiJohn})`)
+} else {
+  console.log(`John's BMI(${bmiJohn} is higher than Mark's BMI(${bmiMark}))`)
+}
+
+console.log("");
