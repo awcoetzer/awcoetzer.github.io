cec1048f4b813cc2c3ad205d8e1e7a2b81a4cc34
Andre Coetzer <coetzeronline@gmail.com>
2022-12-20 09:16:59 +0200

    section-3 lecture 36 completed

--- javascript-pages/course-exercises/javascript-fundamentals-part-2.js
+++ javascript-pages/course-exercises/javascript-fundamentals-part-2.js
@@ -10,7 +10,7 @@
 
 console.log('🦞 LECTURE 32');
 
-console.log("'use strict';");
+// console.log("'use strict';");
 
 console.log('');
 
@@ -19,53 +19,53 @@ console.log('🦞 LECTURE 33');
 
 //  Just logs this string to console
 
-function logger() {
-  console.log('Hello, my name is Andre');
-}
+// function logger() {
+//   console.log('Hello, my name is Andre');
+// }
 
-logger();
-logger();
-logger();
+// logger();
+// logger();
+// logger();
 
-const test = logger();
-console.log(typeof test);
+// const test = logger();
+// console.log(typeof test);
 
-/*
-  Juicer function that allows for demonstration
-  of how functions can accept information and
-  return information.
-*/
-function juicer(nApples, nOranges) {
-  const juice = `Your juice is ${nApples} apples and ${nOranges} Oranges!`;
-  return juice;
-}
+// /*
+//   Juicer function that allows for demonstration
+//   of how functions can accept information and
+//   return information.
+// */
+// function juicer(nApples, nOranges) {
+//   const juice = `Your juice is ${nApples} apples and ${nOranges} Oranges!`;
+//   return juice;
+// }
 
-const myJuice = juicer(3, 4);
+// const myJuice = juicer(3, 4);
 
-console.log(myJuice);
+// console.log(myJuice);
 
 console.log(''); 
 
 //////////////////// Lecture 33 - Assignment ////////////////////
 console.log('🦀 LECTURE 33 - ASSIGNMENT');
 
-function describeCountry(country, population, capitalCity) {
-  const myCountry = `${country} has ${population} million people and it's capital is ${capitalCity}`;
+// function describeCountry(country, population, capitalCity) {
+//   const myCountry = `${country} has ${population} million people and it's capital is ${capitalCity}`;
 
-  return myCountry;
-}
+//   return myCountry;
+// }
 
-const bulgaria = describeCountry('Bulgaria', 7.4, 'Sofia');
-const southAfrica = describeCountry(
-  'South Africa',
-  60,
-  'Pretoria, Bloemfontein and Cape Town',
-);
-const columbia = describeCountry('Columbia', 50, 'Bogota');
+// const bulgaria = describeCountry('Bulgaria', 7.4, 'Sofia');
+// const southAfrica = describeCountry(
+//   'South Africa',
+//   60,
+//   'Pretoria, Bloemfontein and Cape Town',
+// );
+// const columbia = describeCountry('Columbia', 50, 'Bogota');
 
-console.log(bulgaria);
-console.log(southAfrica);
-console.log(columbia);
+// console.log(bulgaria);
+// console.log(southAfrica);
+// console.log(columbia);
 
 console.log('');
 
@@ -73,115 +73,130 @@ console.log('');
 console.log('🦞 LECTURE 34');
 
 // Declaring a function
-const age1 = calcAge1(1989); // invoking function before declaration
-function calcAge1(birthYear) {
-  console.log('Declaring a function');
-  return 2023 - birthYear;
-}
+// const age1 = calcAge1(1989); // invoking function before declaration
+// function calcAge1(birthYear) {
+//   console.log('Declaring a function');
+//   return 2023 - birthYear;
+// }
 
-console.log(age1);
+// console.log(age1);
 
-// Function expression
+// // Function expression
 
-const calcAge2 = function (birthYear) {
-  console.log('Function expression');
-  return 2023 - birthYear;
-};
-const age2 = calcAge2(1989);
+// const calcAge2 = function (birthYear) {
+//   console.log('Function expression');
+//   return 2023 - birthYear;
+// };
+// const age2 = calcAge2(1989);
 
-console.log(age2);
+// console.log(age2);
 
 console.log('');
 
 //////////////////// Lecture 34 - Assignment ////////////////////
 console.log('🦀 LECTURE 34 - ASSIGNMENT');
 
-console.log('--- Declaring Functions ---');
+// console.log('--- Declaring Functions ---');
 
-function percentageOfWorld1(population) {
-  return (population / 7900) * 100;
-}
+// function percentageOfWorld1(population) {
+//   return (population / 7900) * 100;
+// }
 
-const jamaicaPopulation = percentageOfWorld1(3);
-const bulgariaPopulation = percentageOfWorld1(7.4);
-const southAfricaPopulation = percentageOfWorld1(60);
+// const jamaicaPopulation = percentageOfWorld1(3);
+// const bulgariaPopulation = percentageOfWorld1(7.4);
+// const southAfricaPopulation = percentageOfWorld1(60);
 
-console.log(`
-Jamaica's population is ${jamaicaPopulation}% of the world's population,
-Bulgaria's population is ${bulgariaPopulation}% of the world's population,
-South Africa's population is ${southAfricaPopulation}% of the world's population.
-`);
+// console.log(`
+// Jamaica's population is ${jamaicaPopulation}% of the world's population,
+// Bulgaria's population is ${bulgariaPopulation}% of the world's population,
+// South Africa's population is ${southAfricaPopulation}% of the world's population.
+// `);
 
-console.log('--- Functions Expressions ---');
+// console.log('--- Functions Expressions ---');
 
-const percentageOfWorld2 = function () {
-  return (population / 7900) * 100;
-};
+// const percentageOfWorld2 = function () {
+//   return (population / 7900) * 100;
+// };
 
-const americaPopulation = percentageOfWorld1(332);
-const canadaPopulation = percentageOfWorld1(37);
-const mexicoPopulation = percentageOfWorld1(129);
+// const americaPopulation = percentageOfWorld1(332);
+// const canadaPopulation = percentageOfWorld1(37);
+// const mexicoPopulation = percentageOfWorld1(129);
 
-console.log(`
-America's population is ${americaPopulation}% of the world's population,
-Canada's population is ${canadaPopulation}% of the world's population,
-Mexico's population is ${mexicoPopulation}% of the world's population.
-`);
+// console.log(`
+// America's population is ${americaPopulation}% of the world's population,
+// Canada's population is ${canadaPopulation}% of the world's population,
+// Mexico's population is ${mexicoPopulation}% of the world's population.
+// `);
 
 console.log('');
 
 //////////////////// Lecture 35 ////////////////////
 console.log('🦞 LECTURE 35');
 
-const calcAge3 = birthYear => 2023 - birthYear;
-const age3 = calcAge3(1989);
+// const calcAge3 = birthYear => 2023 - birthYear;
+// const age3 = calcAge3(1989);
 
-console.log(age3);
+// console.log(age3);
 
-const yearsBeforeRetirement = (birthYear, firstName) => {
-  const age = 2023 - birthYear;
-  const retirement = 65 - age;
+// const yearsBeforeRetirement = (birthYear, firstName) => {
+//   const age = 2023 - birthYear;
+//   const retirement = 65 - age;
 
-  return `${firstName}'s retirement is in ${retirement} years`;
-};
+//   return `${firstName}'s retirement is in ${retirement} years`;
+// };
 
-console.log(yearsBeforeRetirement(1989, 'Andre'));
+// console.log(yearsBeforeRetirement(1989, 'Andre'));
 
-console.log('');
+// console.log('');
 
-//////////////////// Lecture 35 - Assignment ////////////////////
-console.log('🦀 LECTURE 35 - ASSIGNMENT');
+// //////////////////// Lecture 35 - Assignment ////////////////////
+// console.log('🦀 LECTURE 35 - ASSIGNMENT');
 
-console.log('--- Functions Expressions ---');
-const percentageOfWorld3 = (population) => (population / 7900) * 100;
+// console.log('--- Functions Expressions ---');
+// const percentageOfWorld3 = (population) => (population / 7900) * 100;
 
-const columbiaPopulation = percentageOfWorld1(50);
-const swedenPopulation = percentageOfWorld1(10.4);
-const japanPopulation = percentageOfWorld1(126.17);
+// const columbiaPopulation = percentageOfWorld1(50);
+// const swedenPopulation = percentageOfWorld1(10.4);
+// const japanPopulation = percentageOfWorld1(126.17);
 
-console.log(`
-Columbia's population is ${columbiaPopulation}% of the world's population,
-Sweden's population is ${swedenPopulation}% of the world's population,
-Japan's population is ${japanPopulation}% of the world's population.
-`);
+// console.log(`
+// Columbia's population is ${columbiaPopulation}% of the world's population,
+// Sweden's population is ${swedenPopulation}% of the world's population,
+// Japan's population is ${japanPopulation}% of the world's population.
+// `);
 
 console.log('');
 
 //////////////////// Lecture 36 ////////////////////
 console.log('🦞 LECTURE 36');
 
+// cutting the fruit into pieces
+const fruitPieces = function (fruit) {
+  return fruit * 6;
+}
+
+// juicing the fruiting together using the pieces
+const juicer = function (apples, oranges) {
+  const applePieces = fruitPieces(apples);
+  const orangePieces = fruitPieces(oranges);
+
+  return `Your juice was made with ${applePieces} apple pieces and ${orangePieces} orange pieces.`
+}
+
+console.log(juicer(3, 2));
+
 console.log('');
 
 //////////////////// Lecture 36 - Assignment ////////////////////
 console.log('🦀 LECTURE 36 - ASSIGNMENT');
 
-const describePopulation = function (country, population) {
-  return `${country} has ${population} million people, which is ${percentageOfWorld1(population)}% of the world`
-}
+// const describePopulation = function (country, population) {
+//   return `${country} has ${population} million people, which is ${percentageOfWorld1(population)}% of the world`
+// }
 
-console.log(describePopulation('Bahamas', 397))
-console.log(describePopulation('India', 1350))
-console.log(describePopulation('Brazil', 215))
+// console.log(describePopulation('Bahamas', 397))
+// console.log(describePopulation('India', 1350))
+// console.log(describePopulation('Brazil', 215))
 
 console.log('');
 
