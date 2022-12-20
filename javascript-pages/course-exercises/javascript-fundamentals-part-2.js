f26edacb5d0cfe342e83c32d79dc817c931defae
Andre Coetzer <coetzeronline@gmail.com>
2022-12-20 15:12:10 +0200

    section-3 Coding Challenge - 2 Completed

--- javascript-pages/course-exercises/javascript-fundamentals-part-2.js
+++ javascript-pages/course-exercises/javascript-fundamentals-part-2.js
@@ -365,30 +365,54 @@ console.log('');
 //////////////////// Lecture 40 - Assignment ////////////////////
 console.log('🦀 LECTURE 40 - ASSIGNMENT');
 
-const neighbours = [
-  'Canada',
-  'Mexico'
-];
-console.log(neighbours);
+// const neighbours = [
+//   'Canada',
+//   'Mexico'
+// ];
+// console.log(neighbours);
 
-neighbours.push('Utopia');
-console.log(neighbours);
+// neighbours.push('Utopia');
+// console.log(neighbours);
 
-neighbours.pop();
-console.log(neighbours);
+// neighbours.pop();
+// console.log(neighbours);
 
-if (!neighbours.includes('Germany')) {
-  console.log('Probably not a European country.')
-}
+// if (!neighbours.includes('Germany')) {
+//   console.log('Probably not a European country.')
+// }
 
-neighbours[neighbours.indexOf('Canada')] = 'Conodo';
-console.log(neighbours);
+// neighbours[neighbours.indexOf('Canada')] = 'Conodo';
+// console.log(neighbours);
 
 console.log('');
 
 
 //////////////////// Coding Challenge - 2 ////////////////////
 console.log('🦑 CODING CHALLENGE - 2');
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+};
+
+console.log(calcTip(100));
+
+const bills = [125, 555, 44];
+
+const tips = [
+  calcTip(bills[0]),
+  calcTip(bills[1]),
+  calcTip(bills[2])
+];
+
+console.log(tips);
+
+const total = [
+  bills[0] + tips[0],
+  bills[1] + tips[1],
+  bills[2] + tips[2]
+];
+
+console.log(total);
 
 console.log('');
 
