8834f07d9c28e0498bf7d26a918961c03e493c1c
acoetzer <coetzeronline@gmail.com>
2022-12-29 13:13:39 +0200

    section-7 essentials completed
    still need to change the meta description and add the lectures

--- javascript-pages/course-exercises/dom-and-events-fundamentals.js
+++ javascript-pages/course-exercises/dom-and-events-fundamentals.js
@@ -1,16 +1,56 @@
 'use strict';
 
-//////////////////// Lecture 61 ////////////////////
-console.log('🦞 LECTURE 61');
+//////////////////// Lecture 70 ////////////////////
+console.log('🦞 LECTURE 70');
 
 console.log('');
 
-//////////////////// Lecture 61 - Assignment ////////////////////
-console.log('🦞 LECTURE 61 - ASSIGNMENT');
+//////////////////// Lecture 71 ////////////////////
+console.log('🦞 LECTURE 71');
+
+console.log('');
+
+//////////////////// Lecture 72 ////////////////////
+console.log('🦞 LECTURE 72');
+
+console.log('');
+
+//////////////////// Lecture 73 ////////////////////
+console.log('🦞 LECTURE 73');
+
+console.log('');
+
+//////////////////// Lecture 74 ////////////////////
+console.log('🦞 LECTURE 74');
+
+console.log('');
+
+//////////////////// Lecture 75 ////////////////////
+console.log('🦞 LECTURE 75');
 
 console.log('');
 
 //////////////////// Coding challenge - 1 ////////////////////
 console.log('🦑 CODING CHALLENGE - 1');
+
+console.log('');
+
+//////////////////// Lecture 77 ////////////////////
+console.log('🦞 LECTURE 77');
+
+console.log('');
+
+//////////////////// Lecture 78 ////////////////////
+console.log('🦞 LECTURE 78');
+
+console.log('');
+
+//////////////////// Lecture 79 ////////////////////
+console.log('🦞 LECTURE 79');
+
+console.log('');
+
+//////////////////// Lecture 80 ////////////////////
+console.log('🦞 LECTURE 80');
 
 console.log('');
