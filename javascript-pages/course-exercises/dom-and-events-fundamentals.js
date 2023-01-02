9067c3646bae08aa4192e57ae1d28762caf15fff
acoetzer <coetzeronline@gmail.com>
2023-01-02 19:40:57 +0200

    section-7 project 1 styles added, and functions
    both style and functionality added to the page

--- javascript-pages/course-exercises/dom-and-events-fundamentals.js
+++ javascript-pages/course-exercises/dom-and-events-fundamentals.js
@@ -4,6 +4,63 @@
 console.log('🦞 Project 1');
 console.log('🦞 LECTURE 70 - 77');
 
+const guessButton = document.querySelector('.guess-btn');
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+guessButton.addEventListener('click', () => {
+  let guess = parseInt(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  // Check if there is input
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No Number Chosen';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = "🥳 Yay! That's correct!";
+    document.querySelector('.secret-number').textContent = secretNumber;
+    document.querySelector('.message').style.color = '#4ade80';
+    document.querySelector('.secret-number').style.color = '#4ade80';
+    if (score > highscore) {
+      highscore = score;
+    }
+    document.querySelector('.highscore').textContent = highscore;
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too High!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You Lost';
+      document.querySelector('.message').style.color = '#f87171';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too Low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You Lost';
+      document.querySelector('.message').style.color = '#f87171';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+const againButton = document.querySelector('.again-btn');
+
+againButton.addEventListener('click', function () {
+  score = 20;
+  document.querySelector('.message').textContent = 'Waiting for a guess...';
+  document.querySelector('.message').style.color = 'var(--clr-tint)';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.secret-number').textContent = '?';
+  document.querySelector('.secret-number').style.color = 'var(--clr-tint)';
+  document.querySelector('.guess').value = '';
+})
+
 console.log('');
 
 //////////////////// Lecture 78 ////////////////////
