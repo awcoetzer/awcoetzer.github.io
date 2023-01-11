ce4c7624c6566c28198b96128544777ef642c71b
acoetzer <coetzeronline@gmail.com>
2023-01-11 12:40:57 +0200

    Keep daily activity
    This commit is only to keep daily activity on github.
    Feeling sick and don't want to loose my streak.

--- javascript-pages/course-exercises/javascript-behind-the-scenes.js
+++ javascript-pages/course-exercises/javascript-behind-the-scenes.js
@@ -1,3 +1,13 @@
 'use strict';
 
-console.log('🟢');
+const lectureTextEl = document.querySelectorAll('.lecture-text');
+
+
+for (let i = 0; i < lectureTextEl.length; i++) {
+  lectureTextEl[i].textContent = 'Example text here'
+}
+
+//////////////////// Lecture 89 ////////////////////
+console.log('LECTURE 89');
+
+console.log('');
