d8b196cf5a331590e94dc8fb32aa9dd84e463d7d
acoetzer <coetzeronline@gmail.com>
2023-01-13 14:29:21 +0200

    section-8 lecture 89 Completed

--- javascript-pages/course-exercises/javascript-behind-the-scenes.js
+++ javascript-pages/course-exercises/javascript-behind-the-scenes.js
@@ -1,15 +1,5 @@
 'use strict';
 
-const lectureTextEl = document.querySelectorAll('.lecture-text');
-
-const noCopy = function () {
-  
-}
-
-for (let i = 0; i < lectureTextEl.length; i++) {
-  lectureTextEl[i].textContent = 'Example text here';
-}
-
 //////////////////// Lecture 89 ////////////////////
 console.log('LECTURE 89');
 
