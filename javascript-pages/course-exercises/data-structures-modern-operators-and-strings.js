19d06b5f34d98b0c84349f5f9560c337d30bcf24
acoetzer <coetzeronline@gmail.com>
2023-01-14 13:40:05 +0200

    prepped the js file for section 9

--- javascript-pages/course-exercises/data-structures-modern-operators-and-strings.js
+++ javascript-pages/course-exercises/data-structures-modern-operators-and-strings.js
@@ -1,3 +1,116 @@
 'use strict';
 
-console.log('Hello World');
+//////////////////// Lecture 103 ////////////////////
+console.log('LECTURE 103');
+
+console.log('');
+
+//////////////////// Lecture 104 ////////////////////
+console.log('LECTURE 104');
+
+console.log('');
+
+//////////////////// Lecture 105 ////////////////////
+console.log('LECTURE 105');
+
+console.log('');
+
+//////////////////// Lecture 106 ////////////////////
+console.log('LECTURE 106');
+
+console.log('');
+
+//////////////////// Lecture 107 ////////////////////
+console.log('LECTURE 107');
+
+console.log('');
+
+//////////////////// Lecture 108 ////////////////////
+console.log('LECTURE 108');
+
+console.log('');
+
+//////////////////// Lecture 109 ////////////////////
+console.log('LECTURE 109');
+
+console.log('');
+
+//////////////////// Lecture 110 ////////////////////
+console.log('LECTURE 110');
+
+console.log('');
+
+//////////////////// Lecture 111 ////////////////////
+console.log('LECTURE 111');
+
+console.log('');
+
+//////////////////// Lecture 112 ////////////////////
+console.log('LECTURE 112');
+
+console.log('');
+
+//////////////////// Lecture 113 ////////////////////
+console.log('LECTURE 113');
+
+console.log('');
+
+//////////////////// Lecture 114 ////////////////////
+console.log('LECTURE 114');
+
+console.log('');
+
+//////////////////// Lecture 115 ////////////////////
+console.log('LECTURE 115');
+
+console.log('');
+
+//////////////////// Lecture 116 ////////////////////
+console.log('LECTURE 116');
+
+console.log('');
+
+//////////////////// Lecture 117 ////////////////////
+console.log('LECTURE 117');
+
+console.log('');
+
+//////////////////// Lecture 118 ////////////////////
+console.log('LECTURE 118');
+
+console.log('');
+
+//////////////////// Lecture 119 ////////////////////
+console.log('LECTURE 119');
+
+console.log('');
+
+//////////////////// Lecture 120 ////////////////////
+console.log('LECTURE 120');
+
+console.log('');
+
+//////////////////// Lecture 121 ////////////////////
+console.log('LECTURE 121');
+
+console.log('');
+
+//////////////////// Lecture 122 ////////////////////
+console.log('LECTURE 122');
+
+console.log('');
+
+//////////////////// Lecture 123 ////////////////////
+console.log('LECTURE 123');
+
+console.log('');
+
+//////////////////// Lecture 124 ////////////////////
+console.log('LECTURE 124');
+
+console.log('');
+
+//////////////////// Lecture 125 ////////////////////
+console.log('LECTURE 125');
+
+console.log('');
