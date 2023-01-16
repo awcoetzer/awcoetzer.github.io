50c95f1db39b6d0c197cdae422af6b362c39e1e7
acoetzer <coetzeronline@gmail.com>
2023-01-16 14:25:51 +0200

    section-9 lecture 103 Completed

--- javascript-pages/course-exercises/data-structures-modern-operators-and-strings.js
+++ javascript-pages/course-exercises/data-structures-modern-operators-and-strings.js
@@ -3,6 +3,100 @@
 //////////////////// Lecture 103 ////////////////////
 console.log('LECTURE 103');
 
+// // Made up restaurant menu app
+// const restaurant = {
+//   name: 'Just Delicious',
+//   location: "74-5035 Queen Ka'ahumanu Hwy, Kailua-Kona, Hawaii",
+//   categories: [
+//     'Seafood & Grill',
+//     'Hawaiian BBQ & Grill',
+//     'Hawaiian',
+//     'Organic',
+//   ],
+//   starterMenu: [
+//     'Garlic Bread',
+//     'Bread Sticks',
+//     'Hawaiian Salad',
+//     'Garlic Snails',
+//     'Hot Stone Muscles',
+//   ],
+//   mainMenu: [
+//     'Pizza',
+//     'Seafood Grill',
+//     'Hawaiian Fried Rice & Seafood',
+//     'Seafood Platter with Hawaiian Trio Sauce',
+//     'Ocean Fish, Catch Of The Day',
+//   ],
+//   sidesMenu: [
+//     'French Fries',
+//     'Hawaiian Coleslaw',
+//     'Hawaiian Macaroni Salad',
+//     'Pineapple Rice',
+//     'Hawaiian Rolls',
+//   ],
+//   order: function (starterIndex, mainIndex, sidesIndex) {
+//     return [
+//       this.starterMenu[starterIndex],
+//       this.mainMenu[mainIndex],
+//       this.sidesMenu[sidesIndex],
+//     ];
+//   },
+// };
+
+// // Introduction to deconstructing arrays
+// const arr = [1, 2, 3];
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
+// console.log('--- creating a variable for each array index');
+// console.log(a, b, c);
+
+// console.log('--- Using the deconstruct syntax');
+// const [x, y, z] = arr;
+// console.log(x, y, z);
+
+// // We don't need to select all values when deconstructing
+// // const [first, second] = restaurant.categories;
+
+// // We can even skip the second value
+// console.log('--- Skipping a value');
+
+// const [first, , third] = restaurant.categories;
+// console.log(first, third);
+
+// // Switching variables
+// console.log('--- switching variables');
+
+// let [main, , secondary] = restaurant.categories;
+// console.log(main, secondary);
+
+// // Old way of switch variables using temp
+// // const temp = main;
+// // main = secondary;
+// // secondary = temp
+
+// // using deconstruction as a method of switching
+// [main, secondary] = [secondary, main];
+// console.log(main, secondary);
+
+// // Unpacking a function into variable using deconstruction
+// // const [starterMeal, mainMeal, sideMeal] = restaurant.order(0, 1, 0);
+// // console.log(starterMeal, mainMeal, sideMeal);
+
+// // deconstructing nested arrays
+// const nested = [1, 'skip me', 3, [4, 5]];
+// const [q, , w, [e, r]] = nested;
+// console.log(q, w, e, r);
+
+// // Assigning a default value
+// const mysteryAPI = [8, 9];
+// const [xx = 1, yy = 1, zz = 1] = mysteryAPI;
+// console.log(xx, yy, zz);
+
+// // Practice example, lets say we don't order a side dish
+// const [starterMeal = 'X', mainMeal = 'X', sideMeal = 'X'] = restaurant.order(0, 1);
+// console.log(starterMeal, mainMeal, sideMeal);
+
 console.log('');
 
 //////////////////// Lecture 104 ////////////////////
