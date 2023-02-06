4c1b5c42b91da192b8c96e25e1f559fddcaa6579
acoetzer <coetzeronline@gmail.com>
2023-02-06 10:59:45 +0200

    section-10 JavaScript page prepped and ready

--- javascript-pages/course-exercises/a-closer-look-at-functions.js
+++ javascript-pages/course-exercises/a-closer-look-at-functions.js
@@ -1,3 +1,61 @@
 'use strict';
 
-console.log('Hello');
+//////////////////// Lecture 128 ////////////////////
+console.log('LECTURE 128');
+
+console.log('');
+
+//////////////////// Lecture 129 ////////////////////
+console.log('LECTURE 129');
+
+console.log('');
+
+//////////////////// Lecture 130 ////////////////////
+console.log('LECTURE 130');
+
+console.log('');
+
+//////////////////// Lecture 131 ////////////////////
+console.log('LECTURE 131');
+
+console.log('');
+
+//////////////////// Lecture 132 ////////////////////
+console.log('LECTURE 132');
+
+console.log('');
+
+//////////////////// Lecture 133 ////////////////////
+console.log('LECTURE 133');
+
+console.log('');
+
+//////////////////// Lecture 134 ////////////////////
+console.log('LECTURE 134');
+
+console.log('');
+
+//////////////////// Lecture 135 ////////////////////
+console.log('LECTURE 135');
+
+console.log('');
+
+//////////////////// Lecture 136 ////////////////////
+console.log('LECTURE 136');
+
+console.log('');
+
+//////////////////// Lecture 137 ////////////////////
+console.log('LECTURE 137');
+
+console.log('');
+
+//////////////////// Lecture 138 ////////////////////
+console.log('LECTURE 138');
+
+console.log('');
+
+//////////////////// Lecture 139 ////////////////////
+console.log('LECTURE 139');
+
+console.log('');
